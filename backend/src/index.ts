ccb3f4002a84c600e4a33c8d3c009d8e5f0adc43
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-02 22:02:52 +0700

    add: morgan type

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
+import bodyParser from 'body-parser';
 import express, { Request, Response, Application } from 'express';
+import morgan from 'morgan';
 
 const app: Application = express();
 const port: number = 3000;
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
