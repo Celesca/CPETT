02762e93be632689a4b6423e2c7f2a4cc1840715
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-02 21:48:20 +0700

    setup infrastructure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import express, { Request, Response, Application } from 'express';
+
+const app: Application = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
